26f9aa515d3f2596741bac3de1f032cc271f7aab
Isaac SCHARTIER <ischartier@campus-eni.fr>
2024-07-30 11:47:59 +0200

    Firt demo

---
+++ lib/m_04/m4-app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DemoM4App());
+}
+
+class DemoM4App extends StatelessWidget {
+  const DemoM4App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo M04',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Beurre Doux"),
+      ),
+      body: Column(
+        children: [
+          Text("Ligne 1"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Ligne 2"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            Text("Ligne 3 Col 1"),
+            Text("Ligne 3 Col 2")
+          ],),
+          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"),
+          Row(children: [
+            SizedBox(width: 200, child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat")),
+            Expanded(child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat")),
+          ],)
+        ],
+      ),
+    );
+  }
+}
